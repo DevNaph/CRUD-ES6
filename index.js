f7a3b5809d4b3a47431c298649e2903de531c5fb
Naphtal Chemjor <naphchemjor@outlook.com>
2024-03-18 23:17:40 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const dialog = document.querySelector('dialog');
+const showButton = document.querySelector('#btn-add');
+const close = document.querySelector('#btn-close');
+
+showButton.addEventListener('click', () => {
+    dialog.showModal();
+});
+
+closeButton.addEventListener('click', () => {
+    dialog.close();
+})
